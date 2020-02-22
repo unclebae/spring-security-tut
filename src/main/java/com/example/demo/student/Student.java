a16d3bcc84662347502a2d00168156172555e971
iMac <imac@iMacui-iMac.local>
2020-02-22 23:01:15 +0900

    spring security first

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,15 @@
+package com.example.demo.student;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Student {
+
+    private Integer studentId;
+    private String studentName;
+
+}
